31dedddb454837dc4967f3358de8e60e6c7f952d
viniciusnevescosta <vinicius.nevesco@gmail.com>
2026-04-06 14:56:13 -0300

    [feat] Added a docs button

--- website/astro.config.ts
+++ website/astro.config.ts
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://viniciusnevescosta.com",
+  site: "https://still-orbit.viniciusnevescosta.com",
   integrations: [sitemap()],
 });
